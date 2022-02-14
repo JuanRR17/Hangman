6777aa038a5fccba81735ea9c5ff5c5d6235a06c
JuanRR17 <93441135+JuanRR17@users.noreply.github.com>
2022-02-14 18:41:51 +0100

    not-found-page included

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, 
     Routes, Route } from 'react-router-dom';
 import HangMan from './HangMan';
+import NotFoundPage from './components/NotFoundPage';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HangMan/>}/>
+        <Route path = "*" element={<NotFoundPage/>}/>
       </Routes>
     </Router>
   );
